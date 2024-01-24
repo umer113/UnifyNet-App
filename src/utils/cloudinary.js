f13700e7fb4877b60b671684f954e49b25fe4977
umer113 <umerqadir113@gmail.com>
2024-01-24 12:44:19 +0500

    cloudinary configuration done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary} from cloudinary;
+import fs from "fs";  
+
+// fs = fileSystem 
+// when we delete any file its unlink not delete
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadCloudinary =async(localFilePath)=>{
+  try{
+    if (!localFilePath) return null
+    //upload the file on cloud
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    })
+
+    // file has been uploaded successfully
+    console.log("File is uploaded on cloudinary",response.url)
+
+    return response 
+  }catch(error){
+    fs.unlinkSync(localFilePath) // remove the locally saved temprory file as the operation of uploading got failed
+  }
+}
+
+
+export {uploadCloudinary}
